a65ee24eb10dab8418382a75974dfe9f8ba43604
Hendi Santika <hendisantika@gmail.com>
2021-11-24 20:08:35 +0700

    #31 - Add findAll in UserResource

--- src/main/java/com/hendisantika/resource/UserResource.java
+++ src/main/java/com/hendisantika/resource/UserResource.java
@@ -1,7 +1,12 @@
 package com.hendisantika.resource;
 
+import com.hendisantika.dto.UserDTO;
 import com.hendisantika.service.UserService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,4 +27,18 @@ public class UserResource {
     public UserResource(UserService userService) {
         this.userService = userService;
     }
+
+    @GetMapping
+    public ResponseEntity findAll(@RequestParam(required = false, defaultValue = "0") Integer page,
+                                  @RequestParam(required = false, defaultValue = "3") Integer size,
+                                  @RequestParam(required = false) String[] sort,
+                                  @RequestParam(required = false, defaultValue = "asc") String dir) {
+
+        CollectionModel<UserDTO> users = userService.findAll(page, size, sort, dir);
+        if (users != null) {
+            return ResponseEntity.ok(users);
+        }
+        return ResponseEntity.noContent().build();
+    }
+
 }
