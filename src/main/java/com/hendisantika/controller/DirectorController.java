89c774c4e67bf3a179ac9ec7e607f0cd46145977
Hendi Santika <hendisantika@gmail.com>
2021-11-24 19:39:45 +0700

    #8 - Bug Fix DirectorController

--- src/main/java/com/hendisantika/controller/DirectorController.java
+++ src/main/java/com/hendisantika/controller/DirectorController.java
@@ -1,6 +1,8 @@
 package com.hendisantika.controller;
 
 import com.hendisantika.entity.Director;
+import com.hendisantika.service.DirectorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,7 +24,10 @@ import java.util.List;
  * Time: 19.33
  */
 @RestController
-public class MovieController {
+public class DirectorController {
+    @Autowired
+    private DirectorService directorService;
+
     @GetMapping("/directors")
     public ResponseEntity<CollectionModel<Director>> getAllDirectors() {
         List<Director> directors = directorService.getAllDirectors();
---
+++ src/main/java/com/hendisantika/service/DirectorService.java
@@ -0,0 +1,27 @@
+package com.hendisantika.service;
+
+import com.hendisantika.entity.Director;
+import com.hendisantika.repository.DirectorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-hateoas-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/11/21
+ * Time: 19.37
+ */
+@Service
+public class DirectorService {
+    @Autowired
+    private DirectorRepository directorRepository;
+
+    public List<Director> getAllDirectors() {
+        return directorRepository.findAll();
+    }
+}
