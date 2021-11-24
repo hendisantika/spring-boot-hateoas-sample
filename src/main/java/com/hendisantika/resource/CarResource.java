7803cca9bcaf4b5aa3747d1ad58443548e5eb8ed
Hendi Santika <hendisantika@gmail.com>
2021-11-24 20:06:36 +0700

    #28 - Add CarResource

--- src/main/java/com/hendisantika/resource/CarResource.java
+++ src/main/java/com/hendisantika/resource/CarResource.java
@@ -1,5 +1,9 @@
 package com.hendisantika.resource;
 
+import com.hendisantika.service.CarService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-hateoas-sample
@@ -9,5 +13,13 @@ package com.hendisantika.resource;
  * Date: 24/11/21
  * Time: 19.56
  */
+@RestController
+@RequestMapping("/cars")
 public class CarResource {
+
+    private final CarService carService;
+
+    public CarResource(CarService carService) {
+        this.carService = carService;
+    }
 }
