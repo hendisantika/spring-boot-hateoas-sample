e1be37e6cf97f30c36c748b7d47b42603e7bdc00
Hendi Santika <hendisantika@gmail.com>
2021-11-24 19:58:53 +0700

    #18 - Add UserService

--- src/main/java/com/hendisantika/service/UserService.java
+++ src/main/java/com/hendisantika/service/UserService.java
@@ -1,6 +1,9 @@
 package com.hendisantika.service;
 
-import org.springframework.stereotype.Service;
+import com.hendisantika.dto.CarDTO;
+import com.hendisantika.dto.UserDTO;
+import com.hendisantika.entity.User;
+import org.springframework.hateoas.CollectionModel;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,7 +14,13 @@ import org.springframework.stereotype.Service;
  * Date: 24/11/21
  * Time: 19.55
  */
-@Service
-public class UserService {
+public interface UserService {
 
+    CollectionModel<UserDTO> findAll(int page, int size, String[] sort, String dir);
+
+    UserDTO findByCode(String code);
+
+    CollectionModel<CarDTO> findUserCars(String code);
+
+    UserDTO insert(User user);
 }
