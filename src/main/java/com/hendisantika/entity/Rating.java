da37892ab22470737f359b7f620f10515c0e3f22
Hendi Santika <hendisantika@gmail.com>
2021-11-24 19:36:44 +0700

    #7 - Add Rating Enum

---
+++ src/main/java/com/hendisantika/entity/Rating.java
@@ -0,0 +1,14 @@
+package com.hendisantika.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-hateoas-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/11/21
+ * Time: 19.36
+ */
+public enum Rating {
+    GOOD, NOT_TOO_BAD, BAD
+}
