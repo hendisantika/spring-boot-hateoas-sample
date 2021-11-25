56ce16ed5af20f83fe717478f94ec001f4c48ab9
Hendi Santika <hendisantika@gmail.com>
2021-11-25 12:22:18 +0700

    #55 - Add toModel in AlbumModelAssembler

--- src/main/java/com/hendisantika/assembler/AlbumModelAssembler.java
+++ src/main/java/com/hendisantika/assembler/AlbumModelAssembler.java
@@ -6,6 +6,9 @@ import com.hendisantika.resource.WebController;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-hateoas-sample
@@ -20,5 +23,23 @@ public class AlbumModelAssembler extends RepresentationModelAssemblerSupport<Alb
 
     public AlbumModelAssembler() {
         super(WebController.class, AlbumModel.class);
+    }
+
+    @Override
+    public AlbumModel toModel(AlbumEntity entity)
+    {
+        AlbumModel albumModel = instantiateModel(entity);
+
+        albumModel.add(linkTo(
+                methodOn(WebController.class)
+                        .getActorById(entity.getId()))
+                .withSelfRel());
+
+        albumModel.setId(entity.getId());
+        albumModel.setTitle(entity.getTitle());
+        albumModel.setDescription(entity.getDescription());
+        albumModel.setReleaseDate(entity.getReleaseDate());
+        albumModel.setActors(toActorModel(entity.getActors()));
+        return albumModel;
     }
 }
