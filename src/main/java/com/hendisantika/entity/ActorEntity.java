f459c2d757482414deb1fd3c217f6e8e6e2cb61f
Hendi Santika <hendisantika@gmail.com>
2021-11-25 12:12:53 +0700

    #44 - Add ActorEntity

--- src/main/java/com/hendisantika/entity/ActorEntity.java
+++ src/main/java/com/hendisantika/entity/ActorEntity.java
@@ -1,5 +1,15 @@
 package com.hendisantika.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-hateoas-sample
@@ -9,5 +19,28 @@ package com.hendisantika.entity;
  * Date: 25/11/21
  * Time: 12.11
  */
-public class ActorEntity {
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "albums")
+@Entity
+@Table(name="actor")
+public class ActorEntity implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+
+    @ManyToMany(cascade= CascadeType.ALL)
+    @JoinTable(
+            name = "actor_album",
+            joinColumns = @JoinColumn(name = "actor_id"),
+            inverseJoinColumns = @JoinColumn(name = "album_id"))
+    private List<AlbumEntity> albums;
 }
