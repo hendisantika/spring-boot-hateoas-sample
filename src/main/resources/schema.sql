39b8fd4c277c16ea39db7345ccb69f8de17fec0e
Hendi Santika <hendisantika@gmail.com>
2021-11-25 11:49:14 +0700

    #41 - Add more schema.sql file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -59,3 +59,25 @@ CREATE TABLE hibernate_sequence
 
 INSERT INTO hibernate_sequence
 VALUES (1);
+
+CREATE TABLE actor
+(
+    id         INT PRIMARY KEY,
+    first_name VARCHAR(255) NULL,
+    last_name  VARCHAR(255) NULL,
+    birth_date VARCHAR(255) NULL
+);
+
+CREATE TABLE album
+(
+    id           INT PRIMARY KEY,
+    title        VARCHAR(255) NULL,
+    description  VARCHAR(255) NULL,
+    release_date VARCHAR(255) NULL
+);
+
+CREATE TABLE actor_album
+(
+    actor_id INT,
+    album_id INT
+);
