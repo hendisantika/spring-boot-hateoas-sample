a8c19a8995db5bfc7acd8bd92473aea37d652961
Hendi Santika <hendisantika@gmail.com>
2021-11-25 12:23:43 +0700

    #58 - Add getAllActors in WebController

--- src/main/java/com/hendisantika/resource/WebController.java
+++ src/main/java/com/hendisantika/resource/WebController.java
@@ -2,12 +2,20 @@ package com.hendisantika.resource;
 
 import com.hendisantika.assembler.ActorModelAssembler;
 import com.hendisantika.assembler.AlbumModelAssembler;
+import com.hendisantika.entity.ActorEntity;
 import com.hendisantika.entity.AlbumEntity;
+import com.hendisantika.model.ActorModel;
 import com.hendisantika.repository.ActorRepository;
 import com.hendisantika.repository.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,5 +43,14 @@ public class WebController {
 
     @Autowired
     private PagedResourcesAssembler<AlbumEntity> pagedResourcesAssembler;
+
+    @GetMapping("/api/actors")
+    public ResponseEntity<CollectionModel<ActorModel>> getAllActors() {
+        List<ActorEntity> actorEntities = (List<ActorEntity>) actorRepository.findAll();
+
+        return new ResponseEntity<>(
+                actorModelAssembler.toCollectionModel(actorEntities),
+                HttpStatus.OK);
+    }
 
 }
