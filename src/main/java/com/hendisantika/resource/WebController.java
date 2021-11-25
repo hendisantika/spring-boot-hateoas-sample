ab0a84593650b2219875d30bb9fb9740b087abeb
Hendi Santika <hendisantika@gmail.com>
2021-11-25 12:17:33 +0700

    #53 - Add WebController

--- src/main/java/com/hendisantika/resource/WebController.java
+++ src/main/java/com/hendisantika/resource/WebController.java
@@ -1,5 +1,13 @@
 package com.hendisantika.resource;
 
+import com.hendisantika.assembler.ActorModelAssembler;
+import com.hendisantika.entity.AlbumEntity;
+import com.hendisantika.repository.ActorRepository;
+import com.hendisantika.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-hateoas-sample
@@ -9,5 +17,22 @@ package com.hendisantika.resource;
  * Date: 25/11/21
  * Time: 12.15
  */
+@RestController
 public class WebController {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    @Autowired
+    private ActorRepository actorRepository;
+
+    @Autowired
+    private ActorModelAssembler actorModelAssembler;
+
+    @Autowired
+    private AlbumModelAssembler albumModelAssembler;
+
+    @Autowired
+    private PagedResourcesAssembler<AlbumEntity> pagedResourcesAssembler;
+
 }
